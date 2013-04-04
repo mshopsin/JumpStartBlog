bb9cd45cbb3f4cc836148eaa3ae07490fc7d7798
Brian&Ian <david+luke@appacademy.io>
2013-04-03 17:18:22 -0700

    Login

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -2,8 +2,17 @@ Blogger::Application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
 
+  end
 
+  resources :authors
+  resources :tags
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  match 'login'  => 'author_sessions#new'
+  match 'logout' => 'author_sessions#destroy'
 
 end
